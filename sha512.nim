2775777386b64af4f89a8c789b24d31af105dbb4
xioren <40043405+xioren@users.noreply.github.com>
2024-01-16 23:43:58 -0800

    Update sha512.nim

--- sha512.nim
+++ sha512.nim
@@ -143,6 +143,10 @@ proc update*[T](ctx: var Sha512Context, msg: openarray[T]) =
 
 
 proc finalize*(ctx: var Sha512Context) =
+  # NOTE: compress data in the buffer if it contains more than blockSize - 16 bytes.
+  # this ensures there is room for the length field
+  if ctx.bufferLen >= blockSize - 16:
+    ctx.compress()
   # NOTE: pad the remaining data in the buffer
   ctx.padBuffer()
   # NOTE: process the final block
